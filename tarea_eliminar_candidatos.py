058cb33bddad2b351fbc329e80c06254e2805ee0
Juan E. Lopez <juanmilo5125@gmail.com>
2020-09-20 17:54:19 -0300

    Funcionando con 4 ejemplos (2 V y 2 F) con 3 elementos

--- tarea_eliminar_candidatos.py
+++ tarea_eliminar_candidatos.py
@@ -1,7 +1,58 @@
 import itertools
+
+
+def consistencia(entrenamiento,hipotesis):
+	#hipotesis = ['rojo','?','?']
+	#Cambiar hipotesis
+	#ejemplo = [('azul','cuadrado','grande'),(False)]
+	clasificado = True
+	i = 0
+	for campo in entrenamiento[0]:
+		if(hipotesis[0][i] != campo and hipotesis[0][i] != '?' ):
+			clasificado = False
+			#print(hipotesis[i] , campo)
+			i= i+1
+	if(clasificado == entrenamiento[1]):
+		return True#consistencia
+	else:
+		return False#consistencia
+def comparacion_especifica_pos(entrenamiento,S):
+	
+	for i in range(0,len(S)):
+		if(entrenamiento[i] == S[i] or S[i] == "?"):
+			None
+		elif(entrenamiento[i] != S[i]):
+			aux = list(S)
+			aux[i] = "?"
+			S = tuple(aux)
+	return S
+def clasificacion_general_neg(entrenamiento,G,S):
+	#print(entrenamiento,G)
+	aux = 0
+	g_completo = []
+	j = 0
+	for n in range(0,len(G)):
+		for i in range(0,len(G[n])):
+			if(entrenamiento[i] == G[n][i] or G[n][i] == "?"):
+				aux = aux+1
+				if(G[n][i] == entrenamiento[i]):
+						break
+				else:
+					if(S[i] != entrenamiento[i] and S[i] != "?"):
+						g_aux = list(G[n])
+						g_aux[i] = S[i]
+						g_alt = tuple(g_aux)
+						g_completo.append(g_alt)
+			else:
+				g_aux = list(G[n])
+				g_aux[i] = S[i]
+				g_alt = tuple(g_aux)
+				g_completo.append(g_alt)
+	return(g_completo)
+
 # General(Hipotesis)
 
-G = ['?', '?', '?' ]
+G = [('?', '?', '?' )]
 
 #Especifico(hipotesis)
 
@@ -23,102 +74,53 @@ for salida in lista:
 	list_aux = salida.split(" ")
 	h.append(list_aux)
 
-D1 = [('Rojo','Cuadrado','Grande' ),('Verdadero')]
-D2 = [('Azul', 'Cuadrado', 'Grande' ), ('Verdadero')]
-D3 = [ ('Rojo','Redondo', 'Pequeno' ), ('Falso')]
-D4 = [('Verde', 'Cuadrado','Pequeno' ), ('Falso')]
+D1 = [('Rojo','Cuadrado','Grande' ),(True)]
+D2 = [('Azul', 'Cuadrado', 'Grande' ), (True)]
+D3 = [ ('Rojo','Redondo', 'Pequeno' ), (False)]
+D4 = [('Verde', 'Cuadrado','Pequeno' ), (False)]
 D = [D1 , D2 , D3 , D4]
-def consistencia(entrenamiento):
-	hipotesis = ['rojo','?','?']
-	#Cambiar hipotesis
-	#ejemplo = [('azul','cuadrado','grande'),(False)]
-	clasificado = True
-	for campo in entrenamiento[0]:
-		if(hipotesis[i] != campo and hipotesis[i] != '?' ):
-			clasificado = False
-	if(clasificado == ejemplo[1]):
-		return True#consistencia
-	else:
-		return False#consistencia
 
-	"""
-	for i in range(0,len(hipotesis)):
-		if(hipotesis[i] != ejemplo[0][i] and hipotesis[i] != '?' ):
-			clasificado = False
-	if(clasificado == ejemplo[1]):
-		return True#consistencia
-	else:
-		return False#consistencia
-	"""
-	
-	
-print(consistencia())
-	
-
-
-
-consistente_general = 0
-especifico_positivo = 0
-clasifica_negativo = 0
-generales = []
-aux = []
+primero = 0
 for entrenamiento in D:
-	if(entrenamiento[1] == 'Verdadero'):
-		for i in range(0,(len(G))):			
-			if(entrenamiento[0][i] == G[i] or G[i] == '?'):
-				consistente_general +=1
-			if(S[i] == None):#siempre inconsistente
-				S[i] = entrenamiento[0][i]
-			elif(S[i] == entrenamiento[0][i] or S[i] == '?'):
-				especifico_positivo +=1
-			elif(S[i] != entrenamiento[0][i]):
-				S[i] = '?'
-	elif(entrenamiento[1] == 'Falso'): #problemas NO FUNCIONA
-		for i in range(0,len(S)):
-			if(S[i] != entrenamiento[0][i]):
-				clasifica_negativo +=1
-			if(consistencia(entrenamiento)):
-				
-			else():
-				
-			#if(G[i] == entrenamiento[0][i] or G[i] == '?'):
-			#	if(entrenamiento[0][i] == S[i]):
-			#		print("algo")
-				#elif(entrenamiento[0][i] != S[i]):
-			#general final = < '?' , '?' , grande>		
-
+	if(entrenamiento[1] == True):
+		devuelto = consistencia(entrenamiento,G)
+		#print("devuelve",devuelto)
+		if(devuelto == entrenamiento[1]):
+			print("consistente verdadero , mantengo D")
+		if(primero == 0):
+			primero = 1
+			S = entrenamiento[0]
+		elif(primero != 0):
+			S = comparacion_especifica_pos(entrenamiento[0],S)
+			
+		
+	elif(entrenamiento[1] == False):
+		devuelto = consistencia(entrenamiento,S)
+		if(devuelto == entrenamiento[1]):
+			print("consistente Falso , mantengo D")
+		G = clasificacion_general_neg(entrenamiento[0],G,S)
 print("Especifica",S)
 print("General",G)
-"""parametros_comparados_G = 0
-terminado_G = []
-finalizada_S = []
-finalizada_S = h
-print("\n\n\nVERDADEROS \n\n\n")
-for final in h:
-	#print(len(final))
-	for i in range(0,len(G)):
-		if(G[i] != '?'):
-			if(final[i] == G[i]):
-				parametros_comparados_G = parametros_comparados_G+1	
-	if(parametros_comparados_G == parametros_definidos_G):
-		final.append("Verdadero")
-		terminado_G.append(final)
-		#print(final)
-	parametros_comparados_G = 0
 
-#print(terminado_G)
-
-print("\n\n\nFALSOS \n\n\n")
-terminado_S = []
-parametros_comparados_S = 0
-for final in finalizada_S:
-	for i in range(0,len(S)):
-		if(final[i] == S[i]):
-			parametros_comparados_S = parametros_comparados_S+1
-	if(parametros_comparados_S > 1 and len(final)<7):
-		final.append("Falso")
-		terminado_S.append(final)
-		#print(final)
-	parametros_comparados_S = 0
-#print(terminado_S)
-"""
+#Falsos
+#print(h)
+print(len(h))
+positivo_list = []
+negativo_list = []
+for comparacion in h:
+	positivo = 0 #hago 0 por cada elemento de la lista h
+	for i in range(0,len(comparacion)):
+		if(S[i] == comparacion[i] or S[i] == "?"):
+			positivo = positivo + 1
+		if(G[0][i] == comparacion[i] or G[0][i] == "?"):
+			positivo = positivo + 1
+	if(positivo == 2*len(S)):
+		#print("POSITIVO",comparacion)
+		positivo_list.append(comparacion)
+	else:
+		#print("Negativo",comparacion)
+		negativo_list.append(comparacion)
+print("VERDADERO")
+print(positivo_list)
+print("\n\n FALSOS" )
+print(negativo_list)
